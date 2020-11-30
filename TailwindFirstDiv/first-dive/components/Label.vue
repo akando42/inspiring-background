ee38c485398b9545d4d874723b9d6aee7392c49d
Troy Do <do@infinitylab.world>
2020-11-30 13:58:01 +0700

    64 Robots Samples

---
+++ TailwindFirstDiv/first-dive/components/Label.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="p-6 bg-gray-100 mb-8 text-green-600 rounded-xl shadow-md flex items-center max-w-smm mx-autom space-x-4">
+		<div class="flex-shink-0">
+			<img class="h-12 w-12" src="~/static/chat-repo.svg" alt="Logo">
+		</div>
+		<div>
+			<div class="text-xl font-medium text-black"> Subconcious Programming</div>
+			<p class="text-gray-500"> You have a new message !</p>
+		</div>
+    </div>
+</template>
