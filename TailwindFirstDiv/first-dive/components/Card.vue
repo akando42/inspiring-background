60499dc51b909a29cd82532407b7f753b425104f
Troy Do <do@infinitylab.world>
2020-12-01 13:14:05 +0700

    Updating Card

---
+++ TailwindFirstDiv/first-dive/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="max-w-md mx-auto mt-12 bg-gray-200 rounded-xl shadow-md overflow-hidden md:max-w-3xl">
+			<div class="md:flex">
+				<div class="md:flex-shrink-0">
+					<img 
+					    class="h-52 w-full object-cover md:w-52" 
+					    src="~/static/hakka.jpg" 
+					    alt="Foward-deployed Housing or Just Turtle Up"
+					>
+				</div>
+
+				<div class="p-6 bg-white">
+					<div class="uppercase tracking-wide text-sm text-indigo-600 font-semibold">
+						Case Study
+					</div>
+                    <a href="#" class="block mt-1 text-medium leading-tight font-medium text-black hover:underline">
+                        Hakkak Forward-Deployed Housing in Hostile Territorials
+                    </a>
+                    <p class="mt-2 text-gray-400">
+                    	Hakkak is the people who can not stand boredom since they already traveled for million miles from
+                    	another galaxy. The small familiar enviroment is quickly getting old. The adrenaline rush of adventure and fighting for survival is too much fun.
+                    </p>
+				</div>
+			</div>
+		</div>
+</template>
