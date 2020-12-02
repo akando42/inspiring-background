d8c1c6467e41431eb1ff28445d120ccefb0c2bca
Troy Do <do@infinitylab.world>
2020-12-02 13:26:51 +0700

    Add dist folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+  target: 'dist',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
